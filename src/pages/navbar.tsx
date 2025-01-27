9bd85bcd448d527017f42cff9255ff83195fe0cc
KendallCC <kendall26092002@gmail.com>
2025-01-26 21:17:04 -0600

    update of dropdown options

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -51,11 +51,11 @@ const categories = [
   },
   {
     id: 4,
-    nombre: "Otros",
+    nombre: "Más Productos",
     subcategorias: [
       { id: 18, nombre: "Mochila de Viaje" },
       { id: 19, nombre: "Relojes" },
-      { id: 25, nombre: "Acero" },
+      { id: 25, nombre: "Acero Inoxidable" },
       { id: 26, nombre: "Collares y Pulseras de Mariposa" },
     ],
   },
